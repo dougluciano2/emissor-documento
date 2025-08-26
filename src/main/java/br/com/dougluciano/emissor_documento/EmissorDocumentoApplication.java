8358e702fa6abba34dccfae05c2ac88a35620ecb
Douglas O. Luciano <douglas.luciano2@gmail.com>
2025-08-26 15:44:10 -0300

    Initial commit

---
+++ src/main/java/br/com/dougluciano/emissor_documento/EmissorDocumentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dougluciano.emissor_documento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmissorDocumentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmissorDocumentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dougluciano/emissor_documento/EmissorDocumentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dougluciano.emissor_documento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmissorDocumentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
