f74092f5a2643690b58a22836ebd6ddb1916f9c8
Douglas O. Luciano <douglas.luciano2@gmail.com>
2025-08-26 21:50:26 -0300

    feat: front-end project init

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PersonManagementComponent } from './pages/person-management/person-management.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonManagementComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ front-end/src/app/pages/person-management/person-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-management',
+  templateUrl: './person-management.component.html',
+  styleUrls: ['./person-management.component.css']
+})
+export class PersonManagementComponent {
+
+}
